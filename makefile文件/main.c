b4aec9bc7c41584e5d21bdfbb908fb9707abc6e2
wangguoshuang <1303955416@qq.com>
2018-07-10 10:43:09 +0800

    add makefile

---
+++ makefile文件/main.c
@@ -0,0 +1,15 @@
+/*
+* @Author: admin
+* @Date:   2018-07-10 08:24:21
+* @Last Modified by:   admin
+* @Last Modified time: 2018-07-10 09:36:22
+*/
+# include "mytool1.h"
+# include "mytool2.h"
+# include "stdio.h"
+int main(int argc, char *argv)
+{
+	mytool2_print("hello");
+	mytool1_print("hello");
+	return 0;
+}
---
+++ makefile文件/mytool2.c
@@ -0,0 +1,12 @@
+/*
+* @Author: admin
+* @Date:   2018-07-10 08:24:49
+* @Last Modified by:   admin
+* @Last Modified time: 2018-07-10 09:17:23
+*/
+# include "mytool2.h"
+# include "stdio.h"
+void mytool2_print(char *print_str)
+{
+	printf("This is mytool2 print：%s\n",print_str);
+}
